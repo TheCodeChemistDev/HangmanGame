b8c35294d4352cbdedb8b299daf4b8c2d8961a18
TheKemistLabs <thekemistlabs@gmail.com>
2021-02-22 16:04:09 +0000

    Added methods and tests for getting a new word and a new word of a specified length.

--- app/src/main/java/com/thecodechemist/hangmangame/HangmanGame.java
+++ app/src/main/java/com/thecodechemist/hangmangame/HangmanGame.java
@@ -3,4 +3,27 @@ package com.thecodechemist.hangmangame;
 public class HangmanGame {
 
 
+    public String getNewWord() {
+        return "word";
+    }
+
+    public String getNewWord(int wordLength) {
+
+        switch(wordLength) {
+            case 5:
+                return "bacon";
+            case 6:
+                return "runner";
+            case 7:
+                return "laughed";
+            case 8:
+                return "switched";
+            case 9:
+                return "steadings";
+            case 10:
+                return "incredible";
+            default:
+                return "";
+        }
+    }
 }
--- app/src/test/java/com/thecodechemist/hangmangame/HangmanGameTest.java
+++ app/src/test/java/com/thecodechemist/hangmangame/HangmanGameTest.java
@@ -1,17 +1,38 @@
 package com.thecodechemist.hangmangame;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
 public class HangmanGameTest {
 
+    public static HangmanGame hangmanGame;
+
+    @BeforeClass
+    public static void setup() {
+        hangmanGame = new HangmanGame();
+    }
+
     @Test
     public void test_getWord() {
-        HangmanGame hangmanGame = new HangmanGame();
+
         String word = hangmanGame.getNewWord();
 
         assertEquals("word", word);
     }
+
+    @Test
+    public void test_getWordOfRandomLength() {
+        Random random = new Random();
+        int wordLength = random.nextInt(5) + 5;
+        String word = hangmanGame.getNewWord(wordLength);
+
+        assertEquals(wordLength, word.length());
+    }
+
+
 
 }
